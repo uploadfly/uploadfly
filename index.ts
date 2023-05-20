2201470d9886df0917af01dad180d4d44097633e
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 10:43:44 +0100

    ...

--- index.ts
+++ index.ts
@@ -6,3 +6,13 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 2001;
+
+app.listen(PORT, () => {
+  console.log("Live!");
+});
