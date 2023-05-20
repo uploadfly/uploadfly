b096e639f64a3030f4132187e12efcbf8880732e
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 12:40:06 +0100

    created create API function

---
+++ src/controllers/create-api-key.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+const createApiKey = async (req: Request, res: Response) => {
+  const { owner_id, fly_id } = req.body;
+
+  try {
+  } catch (error) {}
+};
+
+export { createApiKey };
