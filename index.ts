81f254d4dc9b2c7dcf5ca9f638966ae1942c5f51
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 11:59:26 +0100

    test prisma

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import prisma from "./prisma";
+import crypto from "crypto";
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req: Request, res: Response) => {
+  (async () => {
+    await prisma.apiKey.create({
+      data: {
+        key: crypto.randomUUID(),
+        owner_id: crypto.randomUUID(),
+      },
+    });
+    console.log("API key created");
+    res.send("API key created");
+  })();
 });
 
 const PORT = process.env.PORT || 2001;
