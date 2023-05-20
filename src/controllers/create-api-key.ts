bc5e213492e567d70420b3610edd49eee0e09841
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 12:48:16 +0100

    check if fly exist

--- src/controllers/create-api-key.ts
+++ src/controllers/create-api-key.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from "express";
+import prisma from "../../prisma";
 
 const createApiKey = async (req: Request, res: Response) => {
-  const { owner_id, fly_id } = req.body;
+  const { user_id, fly_id } = req.body;
 
   try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "Invalid user id" });
+    }
+
+    const fly = await prisma.fly.findUnique({
+      where: {
+        id: fly_id,
+      },
+    });
+
+    if (!fly) {
+      return res.status(404).json({ message: "Invalid Fly id" });
+    }
   } catch (error) {}
 };
 
