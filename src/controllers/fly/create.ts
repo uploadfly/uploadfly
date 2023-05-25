a308c502f0ebd2acedc5e811b2a9374d3761ac98
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-26 00:22:13 +0100

    check if user exist

--- src/controllers/fly/create.ts
+++ src/controllers/fly/create.ts
@@ -4,11 +4,19 @@ import generate from "boring-name-generator";
 
 const createFly = async (req: Request, res: Response) => {
   const { user_id, name } = req.body;
-  if (!user_id) {
+
+  if (!user_id)
     return res.status(400).json({ message: "Missing user or fly id" });
-  }
 
   try {
+    const isUser = await prisma.user.findUnique({
+      where: {
+        uuid: user_id,
+      },
+    });
+
+    if (!isUser) return res.status(400).json({ message: "Invalid user id" });
+
     const userFlies = await prisma.fly.count({
       where: {
         user_id,
@@ -31,3 +39,5 @@ const createFly = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export { createFly };
