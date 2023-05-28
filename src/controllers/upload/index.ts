5b249dd084b9a42af3b43c8b04c635aeb918ba6b
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 15:10:30 +0100

    aws config

--- src/controllers/upload/index.ts
+++ src/controllers/upload/index.ts
@@ -1,42 +1,31 @@
-import { Request, Response } from "express";
-import prisma from "../../../prisma";
 import AWS from "aws-sdk";
 
-const upload = async (req: Request, res: Response) => {
-  if (!req.headers.authorization) {
-    return res.status(401).json({ message: "Unauthorized request" });
-  }
-  const token = req.headers.authorization.split(" ")[1];
+AWS.config.update({
+  accessKeyId: "YOUR_AWS_ACCESS_KEY",
+  secretAccessKey: "YOUR_AWS_SECRET_ACCESS_KEY",
+  region: "YOUR_AWS_REGION",
+});
 
-  AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-  });
+const s3 = new AWS.S3();
 
-  try {
-    const apiKey = await prisma.apiKey.findUnique({
-      where: {
-        key: token,
-      },
+const uploadFileToS3 = (file: any) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      Bucket: "YOUR_S3_BUCKET_NAME",
+      Key: file.originalname,
+      Body: file.buffer,
+    };
+
+    s3.upload(params, (err: any, data: { Location: unknown }) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log("File uploaded to S3:", data.Location);
+        resolve(data.Location);
+      }
     });
-    if (!apiKey) {
-      return res
-        .status(401)
-        .json({ message: "Unauthorized request. API key is invalid" });
-    }
-
-    if (!apiKey.active) {
-      return res
-        .status(401)
-        .json({ message: "Unauthorized request. API key is inactive" });
-    }
-
-    res.send("Hey babes");
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
+  });
 };
 
-export { upload };
+export { uploadFileToS3 };
