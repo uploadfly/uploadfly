504fb0e04636b61d0166967887a04112f750ec86
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 16:13:30 +0100

    file soze check

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import multer from "multer";
 import { uploadFileToS3 } from "../controllers/upload";
 import prisma from "../../prisma";
+import filesize from "file-size";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -36,15 +37,50 @@ router.post("/", upload.single("file"), async (req: Request, res: Response) => {
       return;
     }
     const file = req.file;
+    const fileSize = parseInt(filesize(file.size).to("MB"));
+
+    if (fileSize > 300) {
+      res.status(400).send("Max file size is 300MB");
+      return;
+    }
+
+    const fly = await prisma.fly.findUnique({
+      where: {
+        uuid: apiKey.fly_id,
+      },
+    });
+
+    const flyStorage = fly?.storage as number;
+    const flyUsedStorage = fly?.used_storage as number;
+
+    console.log({
+      fileSize,
+      flyStorage,
+      flyUsedStorage,
+    });
+
+    if (flyUsedStorage + fileSize > flyStorage) {
+      res.status(403).json({ message: "Storage limit exceeded" });
+      return;
+    }
+
+    await prisma.fly.update({
+      where: {
+        uuid: apiKey.fly_id,
+      },
+      data: {
+        used_storage: flyUsedStorage + fileSize,
+      },
+    });
 
     uploadFileToS3(file)
-      .then((s3Url) => {
-        res.send("File uploaded to S3");
-      })
+      .then((s3Url) => {})
       .catch((err) => {
         console.error(err);
         res.status(500).send("Error uploading file to S3");
       });
+
+    res.send("File uploaded to S3");
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
