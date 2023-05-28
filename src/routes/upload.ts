dff844c345eefc6e11517a51d7feb8478d2b2380
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 15:42:11 +0100

    validation

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,13 +1,36 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 import { uploadFileToS3 } from "../controllers/upload";
+import prisma from "../../prisma";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.post("/", upload.single("file"), (req: Request, res: Response) => {
+router.post("/", upload.single("file"), async (req: Request, res: Response) => {
   try {
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: "Unauthorized request" });
+    }
+    const token = req.headers.authorization.split(" ")[1];
+    const apiKey = await prisma.apiKey.findUnique({
+      where: {
+        key: token,
+      },
+    });
+
+    if (!apiKey) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized request. API key is invalid" });
+    }
+
+    if (!apiKey.active) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized request. API key is inactive" });
+    }
+
     if (!req.file) {
       res.status(400).send("No file uploaded");
       return;
