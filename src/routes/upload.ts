68caaa0654b64a48104744c6322d98e948a589fd
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 15:10:42 +0100

    .

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,8 +1,31 @@
-import expres from "express";
-import { upload } from "../controllers/upload";
+import express, { Request, Response } from "express";
+import multer from "multer";
+import { uploadFileToS3 } from "../controllers/upload";
 
-const router = expres.Router();
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-router.post("/", upload);
+router.post("/", upload.single("file"), (req: Request, res: Response) => {
+  try {
+    if (!req.file) {
+      res.status(400).send("No file uploaded");
+      return;
+    }
+    const file = req.file;
+
+    uploadFileToS3(file)
+      .then((s3Url) => {
+        res.send("File uploaded to S3");
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error uploading file to S3");
+      });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 export { router as uploadRouter };
