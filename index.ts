1046764b28b81b746cdb69a3a24304254ad4d6d2
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 12:27:37 +0100

    installed cookie-parser

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { apiKeyRouter } from "./src/routes/api-key";
 import { flyRouter } from "./src/routes/fly";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("File uploads that fly 🪁");
