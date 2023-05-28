33ef413fbfa91b93f5283a58d026179f3dd312cc
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 17:01:31 +0100

    updated api key gen algorithm

--- src/utils/generateApiKey.ts
+++ src/utils/generateApiKey.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 const generateApiKey = () => {
   const apiKey = uuidv4();
 
-  return apiKey;
+  return apiKey.replaceAll("-", "").substring(0, 32) as string;
 };
 
 export { generateApiKey };
