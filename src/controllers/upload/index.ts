de20347576e897780c09bbc20810f1ff24068a27
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 15:41:54 +0100

    replaced with actual keys

--- src/controllers/upload/index.ts
+++ src/controllers/upload/index.ts
@@ -1,9 +1,13 @@
 import AWS from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
 
 AWS.config.update({
-  accessKeyId: "YOUR_AWS_ACCESS_KEY",
-  secretAccessKey: "YOUR_AWS_SECRET_ACCESS_KEY",
-  region: "YOUR_AWS_REGION",
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  region: AWS_REGION,
 });
 
 const s3 = new AWS.S3();
@@ -11,7 +15,7 @@ const s3 = new AWS.S3();
 const uploadFileToS3 = (file: any) => {
   return new Promise((resolve, reject) => {
     const params = {
-      Bucket: "YOUR_S3_BUCKET_NAME",
+      Bucket: "uploadfly",
       Key: file.originalname,
       Body: file.buffer,
     };
