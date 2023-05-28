f4860640b2166509bc8763eb7425ce644ea18d8e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-28 13:14:42 +0100

    /list

--- src/routes/fly.ts
+++ src/routes/fly.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from "express";
 import { createFly } from "../controllers/fly/create";
+import { getUserFlies } from "../controllers/fly/get-user-flies";
 
 const router = express.Router();
 
 router.post("/create", createFly);
-router.get("/hehe", async (req: Request, res: Response) => {
-  res.send(req.cookies);
-});
+router.get("/list", getUserFlies);
 
 export { router as flyRouter };
