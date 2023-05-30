d2abab357a23a87992e011ce01b12f84e2c29a11
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-30 05:35:55 +0100

    fixed missing file

---
+++ src/utils/generateRandomKey.ts
@@ -0,0 +1,17 @@
+function generateRandomKey(length: number) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomKey = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomKey += characters.charAt(randomIndex);
+  }
+
+  return randomKey;
+}
+
+export { generateRandomKey };
+
+// Written by ChatGPT
+// Modified by @xing0x
