dae0fa503aaf98012c431fddbc437254b5d414d3
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-30 05:32:06 +0100

    ????

--- src/utils/generateApiKey.ts
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from "uuid";
-
-const generateApiKey = () => {
-  const apiKey = uuidv4();
-
-  return apiKey.replaceAll("-", "").substring(0, 32) as string;
-};
-
-export { generateApiKey };
