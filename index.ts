cf9650ecebec42d871fb28ea7b94ed989bbf1f0b
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-30 23:17:07 +0100

    set cors to all origins

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import { uploadRouter } from "./src/routes";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true,
 };
 app.use(cors(corsOptions));
