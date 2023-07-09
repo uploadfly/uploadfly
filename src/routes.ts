a1950c619b81657ca8ec15ef7e2c48f76220996a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-09 12:53:24 +0100

    added authenticateApiKey middleware to "/upload" endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 import multer from "multer";
 import { uploadFile } from "./controllers/upload";
 import { deleteFile } from "./controllers/delete";
+import { authenticateApiKey } from "./middlewares/authenticateApiKey";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.post("/upload", upload.single("file"), uploadFile);
+router.post("/upload", authenticateApiKey, upload.single("file"), uploadFile);
+
 router.delete("/delete", deleteFile);
 
 export { router as uploadRouter };
