9600a3300bb7b6d673c534ce6b3e5cabf6199155
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-09 12:32:01 +0100

    added delete route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 import { uploadFile } from "./controllers/upload";
+import { deleteFile } from "./controllers/delete";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.post("/", upload.single("file"), uploadFile);
+router.post("/upload", upload.single("file"), uploadFile);
+router.delete("/delete", deleteFile);
 
 export { router as uploadRouter };
