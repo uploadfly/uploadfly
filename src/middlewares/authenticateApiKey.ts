a37f3a860319fb9e66d52b4effc9a3c89408df47
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-09 12:45:19 +0100

    created API key authentication middleware

---
+++ src/middlewares/authenticateApiKey.ts
@@ -0,0 +1,46 @@
+import { Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+import { IRequest } from "../interfaces";
+
+const prisma = new PrismaClient();
+
+const authenticateApiKey = async (
+  req: IRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: "Unauthorized request" });
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    const apiKeyByPublicKey = await prisma.apiKey.findUnique({
+      where: {
+        public_key: token,
+      },
+    });
+
+    const apiKeyBySecretKey = await prisma.apiKey.findUnique({
+      where: {
+        secret_key: token,
+      },
+    });
+
+    const apiKey = apiKeyByPublicKey || apiKeyBySecretKey;
+
+    if (!apiKey || !apiKey.active) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized request. API key is invalid" });
+    }
+    req.apiKey = `${apiKey}`;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+export { authenticateApiKey };
