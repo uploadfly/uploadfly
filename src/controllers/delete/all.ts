7690c6de0947c87fc673a2b114003c7b59c471dd
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-12 23:46:43 +0100

    delete all

--- src/controllers/delete/all.ts
+++ src/controllers/delete/all.ts
@@ -1,10 +1,16 @@
 import {
   DeleteObjectCommand,
   DeleteObjectCommandInput,
+  DeleteObjectsCommand,
+  DeleteObjectsCommandInput,
+  ListObjectsCommand,
+  ListObjectsCommandInput,
 } from "@aws-sdk/client-s3";
 import { IRequest } from "../../interfaces";
 import { Response } from "express";
 import prisma from "../../../prisma";
+import { s3Client } from "../../configs/s3";
+import { createInvalidation } from "../../utils/createInvalidation";
 
 const deleteFolder = async (req: IRequest, res: Response) => {
   if (req.apiKey?.key_type === "public") {
@@ -42,6 +48,57 @@ const deleteFolder = async (req: IRequest, res: Response) => {
     });
     return;
   }
+
+  const deleteObjectsInFolder = async (folderPath: string) => {
+    const listParams: ListObjectsCommandInput = {
+      Bucket: "uploadfly",
+      Prefix: folderPath,
+    };
+
+    const listCommand = new ListObjectsCommand(listParams);
+    const { Contents } = await s3Client.send(listCommand);
+
+    if (Contents && Contents.length > 0) {
+      const deleteParams: DeleteObjectsCommandInput = {
+        Bucket: "uploadfly",
+        Delete: {
+          Objects: Contents.map(({ Key }) => ({ Key })),
+          Quiet: false,
+        },
+      };
+
+      const deleteCommand = new DeleteObjectsCommand(deleteParams);
+      await s3Client.send(deleteCommand);
+    }
+  };
+
+  const deleteFolder = async (folderPath: string) => {
+    await deleteObjectsInFolder(folderPath);
+
+    const deleteParams: DeleteObjectCommandInput = {
+      Bucket: "uploadfly",
+      Key: `${folderPath}/`,
+    };
+
+    const deleteCommand = new DeleteObjectCommand(deleteParams);
+    await s3Client.send(deleteCommand);
+
+    await createInvalidation(`/${folderPath}/*`);
+  };
+
+  deleteFolder(folder_id)
+    .then(() => {
+      res.status(200).json({
+        message: "Folder deleted",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: err,
+      });
+      return;
+    });
 };
 
 export { deleteFolder };
