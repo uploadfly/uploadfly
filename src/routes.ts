2d353d0870c6537589a904e329ca6083106d161f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-12 23:45:52 +0100

    added "/delate/all" to routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import { uploadFile } from "./controllers/upload";
 import { deleteFile } from "./controllers/delete";
+import { deleteFolder } from "./controllers/delete/all";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -10,5 +11,7 @@ const upload = multer({ storage });
 router.post("/upload", upload.single("file"), uploadFile);
 
 router.delete("/delete", deleteFile);
+
+router.delete("/delete/all", deleteFolder);
 
 export { router as uploadRouter };
