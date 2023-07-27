c34e8c81e68850451ac8ac5624418ccc712161f8
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-27 02:30:36 +0100

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,36 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import multer from "multer";
 import { uploadFile } from "./controllers/upload";
 import { deleteFile } from "./controllers/delete";
 import { deleteFolder } from "./controllers/delete/all";
+import { authenticateApiKey } from "./middlewares/authenticateApiKey";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.post("/upload", upload.single("file"), uploadFile);
+router.post(
+  "/upload",
+  (req: Request, res: Response, next: NextFunction) =>
+    authenticateApiKey(req, res, next, "/upload"),
 
-router.delete("/delete", deleteFile);
+  upload.single("file"),
+  uploadFile
+);
 
-router.delete("/delete/all", deleteFolder);
+router.delete(
+  "/delete",
+  (req: Request, res: Response, next: NextFunction) =>
+    authenticateApiKey(req, res, next, "/delete"),
+  deleteFile
+);
+
+router.delete(
+  "/delete/all",
+  (req: Request, res: Response, next: NextFunction) =>
+    authenticateApiKey(req, res, next, "/delete/all"),
+
+  deleteFolder
+);
 
 export { router as uploadRouter };
