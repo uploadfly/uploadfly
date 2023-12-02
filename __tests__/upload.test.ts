05327e49a7d08a074fb560d6555ec4cde581e20f
akinloluwami <akinkunmioye42@gmail.com>
2023-12-02 18:06:39 +0100

    create 2 more test cases

--- __tests__/upload.test.ts
+++ __tests__/upload.test.ts
@@ -1,10 +1,39 @@
 import supertest from "supertest";
 import { app } from "../";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const apiKey = {
+  active: process.env.UF_TEST_API_KEY_ACTIVE,
+  inactive: process.env.UF_TEST_API_KEY_INACTIVE,
+  invalid: "***********************************",
+};
 
 describe("/upload", () => {
   it("should return 401 if no API key is provided", async () => {
     const response = await supertest(app).post("/upload");
-    console.log(response.body);
     expect(response.status).toBe(401);
+    expect(response.body.error).toBe(
+      "Unauthorized request. API key is missing."
+    );
+  });
+
+  it("should return 401 if API key is invalid", async () => {
+    const response = await supertest(app)
+      .post("/upload")
+      .set("Authorization", `Bearer ${apiKey.invalid}`);
+    expect(response.status).toBe(401);
+    expect(response.body.error).toBe(
+      "Unauthorized request. API key is invalid."
+    );
+  });
+
+  it("should return 401 if API key has been deactivated", async () => {
+    const response = await supertest(app)
+      .post("/upload")
+      .set("Authorization", `Bearer ${apiKey.inactive}`);
+    expect(response.status).toBe(401);
+    expect(response.body.error).toBe("API key has been deactivated.");
   });
 });
